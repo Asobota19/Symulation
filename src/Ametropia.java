d681b38997b83778b68d005da44215a2f472ba51
GustawWrzesinski <48773645+GustawWrzesinski@users.noreply.github.com>
2019-06-06 22:39:28 +0200

    Update Ametropia.java

--- src/Ametropia.java
+++ src/Ametropia.java
@@ -1,22 +1,31 @@
 public class Ametropia {
 
-	public double setFocus (int wada, boolean rodzaj)
-	{
-		
-		double AVpowofeye = 58.64; 
-		double firstfocos, focos,powofeye;
-		firstfocos = 1/AVpowofeye;
-		if(rodzaj == false) {  // nadwzrocznosc
-		powofeye = AVpowofeye + wada;
-		focos = firstfocos - 1/powofeye; //od tego miejsca widzi do nieskonczonosci
-		}
-		else {	//krotkowidz
-			powofeye = AVpowofeye - wada;
-			focos = firstfocos - 1/powofeye; // od tego miejsca nie widzi do nieskończoności
-		}
-		return focos*100;
+	Symulation symulacja;
+
+	public Ametropia(Symulation symulacja){	
+		this.symulacja = symulacja;
 		
 	}
 	
+	public int setFocus (int wada)
+	{
+		int focos;
+		double AVpowofeye = 58.64; 
+		double firstfocos, focosf,powofeye;
+		firstfocos = 1/AVpowofeye;
+		
+		if(symulacja.farSightedness.isSelected()) {  // nadwzrocznosc
+		powofeye = AVpowofeye + wada;
+		focosf = (firstfocos - 1/powofeye)*100000; //od tego miejsca widzi do nieskonczonosci
+		}
+		else {	//krotkowidz
+			powofeye = AVpowofeye + wada;
+			focosf = (firstfocos - 1/powofeye)*100000; // od tego miejsca nie widzi do nieskoĹ„czonoĹ›ci
+		}
+		focos = (int) Math.round(focosf);
+		
+		return focos;
+		
+	}
 	
 }
